9a6ed1d75e3220541d539a799d51545e728d01df
Aditya <adityaupd.prof@gmail.com>
2025-03-18 02:48:35 +0530

    integrating the files

--- src/Aap.java
+++ src/Aap.java
@@ -6,7 +6,12 @@ public class Aap {
         @Override
             public void run(){
                 new MusicPlayerGUI().setVisible(true);
-            }
+
+//                Song song = new Song("assets/Wind Riders - Asher Fulero.mp3");
+//                System.out.println(song.getSongTitle());
+//                System.out.println(song.getSongArtist());
+
+        }
         });
     }
 }
--- src/musicPlayer.java
+++ src/musicPlayer.java
@@ -1,5 +1,36 @@
+import javazoom.jl.player.advanced.AdvancedPlayer;
+
+import java.io.FileInputStream;
+
 public class musicPlayer {
     // this class will store the song details
-
     private  Song currentSong;
+
+    // useing JPlayer library to create an advanced object which will handle paying the music
+    private AdvancedPlayer advancedPlayer;
+
+
+    //constructor
+    public musicPlayer(){
+
+    }
+
+    public void loadSong(Song song){
+        currentSong = song;
+
+        // play the curretn song if not null
+        if(currentSong != null){
+            playCurrentSong();
+        }
+    }
+
+    public void playCurrentSong(){
+        try{
+            //read  mp3 audio data
+            FileInputStream fileInputStream =  new FileInputStream(currentSong.getFilePath());
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
